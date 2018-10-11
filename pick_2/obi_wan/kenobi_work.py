732652780bf8be4187a6290cdc158b9a93d1b97c
Nick Dunphy <2020ndunphy@dwight.edu>
2018-10-11 13:10:09 -0400

    finished_variables

--- pick_2/obi_wan/kenobi_work.py
+++ pick_2/obi_wan/kenobi_work.py
@@ -1,9 +1,13 @@
 #I pushed this to git!
-#variables
-#A variable is something that can change based on information inside the program
+# variables
+# A variable is something that can change based on information inside the program
 
 # Mild
 # input "truth or dare"
 # return "truth" or "dare"
+
 # Medium
-#
+# get computer to give a question based on choice of "truth" or "dare"
+
+#Spicy
+# play a full game of truth or dare with the computer
